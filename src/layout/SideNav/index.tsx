c33f2f41a11676d787aaccc533393dbf3c705e93
gtr <gtrx32@bk.ru>
2024-02-28 17:52:54 +0300

    side menu arrow fix

--- src/layout/SideNav/index.tsx
+++ src/layout/SideNav/index.tsx
@@ -1,6 +1,7 @@
 import Button from "../../components/UI/Button";
 import s from "./SideNav.module.scss";
-import arrow from "../../assets/navHideButton.svg";
+import navHide from "../../assets/navHideButton.svg";
+import navShow from "../../assets/navShowButton.svg";
 import Container from "../../components/UI/Container";
 import SideNavList from "./SideNavList";
 import { useState } from "react";
@@ -15,8 +16,8 @@ const SideNav = () => {
     <div className={clsx(s.sideNav, sideMenuIsOpen && s.openedNav)}>
       <div className={s.topRow}>
         {sideMenuIsOpen && <Container>ADMIN</Container>}
-        <Button onClick={handleSideMenuClick} className={clsx(s.button, s.buttonClose)}>
-          <img src={arrow} alt="" />
+        <Button onClick={handleSideMenuClick} className={s.button}>
+          <img src={sideMenuIsOpen ? navHide : navShow} alt="" />
         </Button>
       </div>
       {sideMenuIsOpen && <SideNavList />}
