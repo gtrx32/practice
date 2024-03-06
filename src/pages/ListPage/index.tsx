f51867c1c62da5314df061a3594638a3eaf4f391
gtr <gtrx32@bk.ru>
2024-03-06 18:40:10 +0300

    listpage styling

--- src/pages/ListPage/index.tsx
+++ src/pages/ListPage/index.tsx
@@ -3,6 +3,12 @@ import { useState, useEffect } from "react";
 import { Tables, Columns } from "./types";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import SearchField from "../../components/UI/SearchField";
+import Button from "../../components/UI/Button";
+import pencil from "../../assets/table/pencil.svg";
+import trashcan from "../../assets/table/trashcan.svg";
+import sideArrow from "../../assets/table/sideArrow.svg";
+import sideArrowGray from "../../assets/table/sideArrowGray.svg";
 
 interface ListPageProps {
   table: string;
@@ -19,13 +25,55 @@ const ListPage: React.FC<ListPageProps> = ({ table }) => {
       });
   }, [table]);
 
+  const actionsBodyTemplate = () => {
+    return (
+      <div className={s.buttons}>
+        <Button className={s.button}>
+          <img src={pencil} alt="" />
+        </Button>
+        <Button className={s.button}>
+          <img src={trashcan} alt="" />
+        </Button>
+      </div>
+    );
+  };
+
+  const arrowTemplate = (isHeader: boolean) => {
+    return (
+      <div className={s.sideArrow}>
+        <img src={isHeader ? sideArrowGray : sideArrow} />
+      </div>
+    );
+  };
+
   return (
     <div className={s.container}>
-      <h1 className={s.title}>{Tables[table as keyof typeof Tables]}</h1>
-      <DataTable value={data} tableStyle={{ minWidth: "50rem" }}>
+      <div className={s.topRow}>
+        <h1 className={s.title}>{Tables[table as keyof typeof Tables]}</h1>
+        <SearchField className={s.search} />
+      </div>
+      <DataTable value={data}>
+        <Column
+          header={() => arrowTemplate(true)} // Передаем параметр true для заголовка
+          body={() => arrowTemplate(false)}
+          headerClassName="helpme"
+          style={{ width: `40px`, maxWidth: `40px` }}
+        />
         {Columns[table as keyof typeof Columns].map((column) => (
-          <Column field={column.field} header={column.header}></Column>
+          <Column
+            key={column.field}
+            header={column.header}
+            field={column.field}
+            sortable
+            style={{ width: column.width, maxWidth: column.width }}
+          ></Column>
         ))}
+        <Column
+          header="Действия"
+          body={actionsBodyTemplate}
+          style={{ width: `90px`, maxWidth: `90px`, textAlign: `center` }}
+          headerClassName="actions"
+        />
       </DataTable>
     </div>
   );
