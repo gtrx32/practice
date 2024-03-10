3a93826e283f4b7f09d6b9e41e40059137501da1
gtrx32 <gtrx32@bk.ru>
2024-03-10 19:34:53 +0300

    -fetch +axios

--- src/pages/ListPage/index.tsx
+++ src/pages/ListPage/index.tsx
@@ -6,50 +6,57 @@ import { DataTable } from "primereact/datatable";
 import ActionsBodyTemplate from "./_components/ActionsBodyTemplate";
 import UpperPanel from "./_components/UpperPanel";
 import LoadingSpinner from "../../components/LoadingSpinner";
+import axios from "axios";
 
 interface ListPageProps {
   table: string;
 }
 
 const ListPage: React.FC<ListPageProps> = ({ table }) => {
-  const [data, setData] = useState<[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/" + table)
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json);
-        setIsLoading(false);
-      });
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/" + table)
+      .then(({ data }) => setData(data))
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
   }, [table]);
 
   return (
     <div className={s.container}>
       <UpperPanel table={table} />
-      {!isLoading ? (
+      {isError && <p>Произошла ошибка при загрузке данных</p>}
+      {!isError && (
         <>
-          <DataTable value={data.slice(0, 20)} scrollable>
-            {Columns[table as keyof typeof Columns].map((column) => (
-              <Column
-                key={column.field}
-                header={column.header}
-                field={column.field}
-                sortable
-                style={{ width: column.width, maxWidth: column.width }}
-              ></Column>
-            ))}
-            <Column
-              header="Действия"
-              body={<ActionsBodyTemplate />}
-              headerClassName="actions"
-              style={{ width: `90px`, maxWidth: `90px`, textAlign: `center` }}
-            />
-          </DataTable>
+          {!isLoading ? (
+            <>
+              <DataTable value={data.slice(0, 20)} scrollable>
+                {Columns[table as keyof typeof Columns].map((column) => (
+                  <Column
+                    key={column.field}
+                    header={column.header}
+                    field={column.field}
+                    sortable
+                    style={{ width: column.width, maxWidth: column.width }}
+                  ></Column>
+                ))}
+                <Column
+                  header="Действия"
+                  body={<ActionsBodyTemplate />}
+                  headerClassName="actions"
+                  style={{ width: `90px`, maxWidth: `90px`, textAlign: `center` }}
+                />
+              </DataTable>
+            </>
+          ) : (
+            <LoadingSpinner />
+          )}
         </>
-      ) : (
-        <LoadingSpinner />
       )}
     </div>
   );
