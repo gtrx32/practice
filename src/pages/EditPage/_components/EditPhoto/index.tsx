2bde4d527229d31b15b1c6c0997eb1401b7bceda
gtrx32 <gtrx32@bk.ru>
2024-03-11 22:36:22 +0300

    image uploader

--- src/pages/EditPage/_components/EditPhoto/index.tsx
+++ src/pages/EditPage/_components/EditPhoto/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import ComboBox from "../../../../components/UI/ComboBox";
 import SaveButton from "../../../../components/UI/SaveButton";
 import TextBox from "../../../../components/UI/TextBox";
@@ -6,6 +7,13 @@ import s from "./EditPhoto.module.scss";
 interface EditPhotoProps {}
 
 const EditPhoto: React.FC<EditPhotoProps> = () => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const handleImageChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const file = target.files?.[0];
+    setSelectedImage(file || null);
+  };
+
   return (
     <div className={s.form}>
       <div className={s.block}>
@@ -16,8 +24,17 @@ const EditPhoto: React.FC<EditPhotoProps> = () => {
           Выберите альбом
         </ComboBox>
       </div>
+      <div className={s.formImage}>
+        <h2 className={s.imageTitle}>Изображение</h2>
+        <div className={s.image}>
+          <label className={s.imageLabel} htmlFor="imageInput">
+            Перетащите сюда или нажмите для выбора
+          </label>
+          <input type="file" id="imageInput" className={s.imageUploader} onChange={handleImageChange} />
+          {selectedImage && <img src={URL.createObjectURL(selectedImage)} className={s.imagePreview} />}
+        </div>
+      </div>
       <SaveButton>Сохранить изменения &#62;&#62;&#62; </SaveButton>
-      {/*блок для загрузки картинки*/}
     </div>
   );
 };
