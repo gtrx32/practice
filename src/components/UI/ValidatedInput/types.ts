9ae04487325b4b8c50d23cb842a696c985b8f787
gtr <gtrx32@bk.ru>
2024-03-18 14:50:43 +0300

    regexps & sidemenu links animation fix

--- src/components/UI/ValidatedInput/types.ts
+++ src/components/UI/ValidatedInput/types.ts
@@ -7,10 +7,9 @@ type Pattern = {
   default: RegExp;
   none: RegExp;
 };
-
 export const patterns: Pattern = {
-  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-  phone: /^[\d-]+$/,
+  email: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/,
+  phone: /^(?:(?:\d{1,2}-)?\(?\d{3}\)?[-. ]?)?\d{3}[-. ]?\d{4}(?:\s*(?:x|ext)\s*\d+)?$/,
   id: /^\d+$/,
   default: /.+/,
   none: /^.*/,
