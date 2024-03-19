f93177dc251370536296ff04effa89d86f17bbb7
gtr <gtrx32@bk.ru>
2024-03-19 11:34:37 +0300

    pagination complete

--- src/pages/ListPage/_components/Pagination/index.tsx
+++ src/pages/ListPage/_components/Pagination/index.tsx
@@ -14,6 +14,8 @@ const Pagination: React.FC<PaginationProps> = ({
 }) => {
   const [pagesCount, setPagesCount] = useState<number>(1);
   const [displayedPages, setDisplayedPages] = useState<number[]>([]);
+  const [showInput, setShowInput] = useState<boolean>(false);
+  const [customPage, setCustomPage] = useState<string>(currentPage.toString());
 
   const generateDisplayedPages = (currentPage: number, pageCount: number) => {
     const firstPage = 1;
@@ -44,34 +46,68 @@ const Pagination: React.FC<PaginationProps> = ({
 
   useEffect(() => {
     generateDisplayedPages(currentPage, pagesCount);
+    setCustomPage(currentPage.toString());
   }, [pagesCount, currentPage]);
 
-  const handlePrevPageClick = () => {
-    currentPage > 1 && setCurrentPage(currentPage - 1);
-  };
+  const handlePrevPageClick = () => currentPage > 1 && setCurrentPage(currentPage - 1);
+  const handleNextPageClick = () => currentPage < pagesCount && setCurrentPage(currentPage + 1);
 
-  const handleNextPageClick = () => {
-    currentPage < pagesCount && setCurrentPage(currentPage + 1);
-  };
-
-  const handleRowsOnPageChange = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) =>
+  const handleRowsPerPageChange = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) =>
     setRowsPerPage(Number(value));
+
+  const handleCustomPageChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => setCustomPage(value);
+  const handleCustomPageSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const pageNumber = parseInt(customPage);
+    if (isNaN(pageNumber)) setShowInput(false);
+    else if (pageNumber >= 1 && pageNumber <= pagesCount) {
+      setCurrentPage(pageNumber);
+      setCustomPage(currentPage.toString());
+      setShowInput(false);
+    }
+  };
 
   return (
     <div className={s.wrapper}>
-      <div className={s.pagination}>
-        <Button onClick={handlePrevPageClick}>&#60; Назад</Button>
-        {displayedPages.map((page, index) => (
-          <Button onClick={() => setCurrentPage(page)} key={index}>
-            {page !== -1 ? page : "..."}
-          </Button>
-        ))}
-        <Button onClick={handleNextPageClick}>Далее &#62;</Button>
-      </div>
+      {!showInput ? (
+        <div className={s.pagination}>
+          <Button onClick={handlePrevPageClick}>&#60; Назад</Button>
+          {displayedPages.map((page, index) =>
+            page !== -1 ? (
+              <Button
+                className={page === currentPage ? s.currentPage : ""}
+                disabled={page === currentPage}
+                onClick={() => setCurrentPage(page)}
+                key={index}
+              >
+                {page}
+              </Button>
+            ) : (
+              <Button onClick={() => setShowInput(true)} key={index}>
+                ...
+              </Button>
+            )
+          )}
+          <Button onClick={handleNextPageClick}>Далее &#62;</Button>
+        </div>
+      ) : (
+        <form onSubmit={handleCustomPageSubmit} className={s.pagination}>
+          <Button type="submit">Перейти</Button>
+          <input
+            className={s.formInput}
+            type="number"
+            min={1}
+            max={pagesCount}
+            value={customPage}
+            onChange={handleCustomPageChange}
+          />
+          <Button type="submit">&#10006;</Button>
+        </form>
+      )}
       <div className={s.rows}>
         <div className={s.rowsPerPage}>
           Строк на странице:
-          <select className={s.selectCount} value={rowsPerPage} onChange={handleRowsOnPageChange}>
+          <select className={s.selectCount} value={rowsPerPage} onChange={handleRowsPerPageChange}>
             {[5, 10, 15, 20].map((option) => (
               <option key={option} value={option}>
                 {option}
