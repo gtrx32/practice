d215e445963e3336a06b44f812f696dfa6d08735
gtr <gtrx32@bk.ru>
2024-03-25 11:49:20 +0300

    login button

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,17 +1,25 @@
 import s from "./Header.module.scss";
 import Container from "../../components/UI/Container";
 import Button from "../../components/UI/Button";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import MenuIsOpenContext from "../../context/MenuIsOpenContext";
 import ThemeContext from "../../context/ThemeContext";
 import { Logo, ProfileIcon, ThemeIcon } from "../../assets/images/icons";
+import clsx from "clsx";
 
 const Header = () => {
-  const { menuIsOpen, setMenuIsOpen } = useContext(MenuIsOpenContext);
   const { theme, setTheme } = useContext(ThemeContext);
 
+  const { menuIsOpen, setMenuIsOpen } = useContext(MenuIsOpenContext);
   const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
+
+  const { pathname } = useLocation();
+  const [profileMenuIsOpen, setProfileMenuIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setProfileMenuIsOpen(false);
+  }, [pathname]);
 
   return (
     <Container as="header" className={s.header}>
@@ -23,9 +31,16 @@ const Header = () => {
           <Button onClick={() => setTheme(theme === "light" ? "dark" : "light")} aria-label="theme">
             <ThemeIcon />
           </Button>
-          <Button aria-label="profile">
-            <ProfileIcon />
-          </Button>{" "}
+          {pathname !== "/login" && (
+            <div className={s.loginButton}>
+              <Button aria-label="profile" onClick={() => setProfileMenuIsOpen(!profileMenuIsOpen)}>
+                <ProfileIcon />
+              </Button>
+              <div className={clsx(s.logout, profileMenuIsOpen ? s.open : s.close)}>
+                <Link to="/login">Выйти</Link>
+              </div>
+            </div>
+          )}
         </div>
         <Button onClick={handleMenuClick} className={s.mobileMenuButton}>
           {menuIsOpen ? "Закрыть" : "Меню"}
