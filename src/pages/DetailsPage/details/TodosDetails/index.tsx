b2e76b2c9432dcb31d3b49317a940e05f053afa6
gtrx32 <gtrx32@bk.ru>
2024-04-11 22:52:06 +0300

    details page completed

--- src/pages/DetailsPage/details/TodosDetails/index.tsx
+++ src/pages/DetailsPage/details/TodosDetails/index.tsx
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import DetailsDataContext from "../../../../context/DetailsDataContext";
+import DataRow from "../../_components/DataRow";
+
 const TodosDetails = () => {
-  return <div />;
+  const { data, relatedData, relatedPath } = useContext(DetailsDataContext);
+
+  return (
+    <div>
+      <DataRow name="id" content={(data as TodoType).id} type="text" />
+      <DataRow name="Автор" content={(relatedData as UserType).name} type="link" url={relatedPath} />
+      <DataRow name="Текст" content={(data as TodoType).title} type="text" />
+      <DataRow name="Выполнена" content={(data as TodoType).completed} type="boolean" />
+    </div>
+  );
 };
 
 export default TodosDetails;
--- src/pages/DetailsPage/details/UsersDetails/index.tsx
+++ src/pages/DetailsPage/details/UsersDetails/index.tsx
@@ -1,5 +1,37 @@
+import { useContext } from "react";
+import DetailsDataContext from "../../../../context/DetailsDataContext";
+import DataRow from "../../_components/DataRow";
+
 const UsersDetails = () => {
-  return <div />;
+  const { data } = useContext(DetailsDataContext);
+
+  return (
+    <div>
+      <DataRow name="id" content={(data as UserType).id} type="text" />
+      <DataRow name="ФИО" content={(data as UserType).name} type="text" />
+      <DataRow name="Никнейм" content={(data as UserType).username} type="text" />
+      <DataRow name="E-mail" content={(data as UserType).email} type="email" />
+      <DataRow name="Телефон" content={(data as UserType).phone} type="text" />
+      <DataRow
+        name="Адрес"
+        content={
+          (data as UserType).address.street +
+          ", " +
+          (data as UserType).address.city +
+          ", " +
+          (data as UserType).address.zipcode
+        }
+        type="text"
+      />
+      <DataRow name="Название компании" content={(data as UserType).company.name} type="text" />
+      <DataRow
+        name="Веб-сайт"
+        content={(data as UserType).website}
+        type="link"
+        url={"https://" + (data as UserType)?.website}
+      />
+    </div>
+  );
 };
 
 export default UsersDetails;
